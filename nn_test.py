32ea3c821e83dfc76da20786b2fb78a19e481e89
NabilTalukder <71780298+NabilTalukder@users.noreply.github.com>
2023-11-15 22:26:14 +0000

    testing to see if neural network code uploads

---
+++ nn_test.py
@@ -0,0 +1,17 @@
+#importing libraries
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#importing data
+from .data_utils import *
+
+'''
+use the source jonathan used to load datasets, not the github version control
+stuff yet
+Focus on just getting the data into this file and building the neural netwrork
+based on the labs
+'''
+print("done")
+sayHi()
